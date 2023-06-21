6bc2bcd545249a3eeb381d53bd4858baa5a94cc4
Joana Santos <97113028+joanamcs@users.noreply.github.com>
2023-06-21 10:04:03 +0100

    feat(theme-context): add theme provider and context (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 "use client"
 
-import { useState } from "react"
-import themes from "@/themes/index.json"
-
 import { Badge } from "@/components/ui/badge"
-import { Button, buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
@@ -14,66 +11,64 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
-import ThemeSwitcher from "@/components/theme-switcher"
+import {useTheme} from "@/components/shadcn-theme-provider";
+import {ThemeArrows} from "@/components/theme-arrows";
 
 export default function IndexPage() {
-  const [currentTheme, setCurrentTheme] = useState({
-    index: 0,
-    ...themes[0],
-  })
+  const { currentTheme } = useTheme()
+
+  if (!currentTheme) {
+    return null
+  }
 
   return (
-    <>
-      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-        <div className="flex max-w-[980px] flex-col items-start gap-2">
-          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-            {currentTheme.name} <br className="hidden sm:inline" />
-          </h1>
-          <p className="max-w-[700px] text-lg text-muted-foreground">
-            {currentTheme.description}
-          </p>
-        </div>
-        <ThemeSwitcher
-          currentTheme={currentTheme}
-          setCurrentTheme={setCurrentTheme}
-        />
-      </section>
-      <section className="container grid grid-cols-3 items-center gap-6 pb-8 pt-6 md:py-10">
-        <div className="grid gap-3">
-          <Button>Primary</Button>
-          <Button variant="secondary">Secondary</Button>
-          <Button variant="destructive">Destructive</Button>
-          <Button variant="outline">Outline</Button>
-          <Button variant="ghost">Ghost</Button>
-        </div>
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-        <div>
-          <div className="flex gap-3 pb-8">
-            <Badge>Default</Badge>
-            <Badge variant="secondary">Secondary</Badge>
-            <Badge variant="outline">Outline</Badge>
-            <Badge variant="destructive">Destructive</Badge>
+    <ThemeArrows>
+        <section className="container grid items-center gap-6 pb-8 pt-6 md:py-5">
+          <div className="flex max-w-[980px] flex-col items-start gap-2">
+            <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+              {currentTheme.name} <br className="hidden sm:inline" />
+            </h1>
+            <p className="max-w-[700px] text-lg text-muted-foreground">
+              {currentTheme.description}
+            </p>
           </div>
-          <div className="flex items-center space-x-4">
-            <Skeleton className="h-12 w-12 rounded-full" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
+        </section>
+        <section className="container grid grid-cols-1 md:grid-cols-3 items-center gap-6 pb-8 pt-6 md:py-10">
+          <div className="grid gap-3">
+            <Button>Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="destructive">Destructive</Button>
+            <Button variant="outline">Outline</Button>
+            <Button variant="ghost">Ghost</Button>
+          </div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+          <div>
+            <div className="flex flex-col md:flex-row gap-3 pb-8">
+              <Badge>Default</Badge>
+              <Badge variant="secondary">Secondary</Badge>
+              <Badge variant="outline">Outline</Badge>
+              <Badge variant="destructive">Destructive</Badge>
+            </div>
+            <div className="flex items-center space-x-4">
+              <Skeleton className="h-12 w-12 rounded-full" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </>
+        </section>
+    </ThemeArrows>
   )
 }
---
+++ components/theme-arrows.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import * as React from "react"
+import {ReactNode} from "react";
+import {ChevronLeft, ChevronRight} from "lucide-react";
+import {useTheme} from "@/components/shadcn-theme-provider";
+
+export function ThemeArrows({ children }: { children: ReactNode}) {
+  const { nextTheme, previousTheme } = useTheme()
+
+  return (
+    <div className={'relative'}>
+      <div className={'md:block'}>
+        <ChevronLeft className={'text-primary absolute left-0 top-16 md:top-52 w-10 h-10 md:w-20 md:h-20'} onClick={previousTheme} />
+        <ChevronRight className={'text-primary absolute right-0 top-16 md:top-52 w-10 h-10 md:w-20 md:h-20'} onClick={nextTheme} />
+      </div>
+      {children}
+    </div>
+  )
+}
